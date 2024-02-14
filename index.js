2637212f31db6d40b463464353bf750542753d0f
DanielyHernandez <daniely.ch@hotmail.com>
2024-02-14 20:09:42 -0300

    commit desafio classes de um jogo

---
+++ index.js
@@ -0,0 +1,32 @@
+class Heroi {
+    constructor(nome, idade, tipo) {
+        this.nome = nome;
+        this.idade = idade;
+        this.tipo = tipo;
+    }
+
+    atacar() {
+        let ataque;
+        if (this.tipo === "mago") {
+            ataque = "magia";
+        } else if (this.tipo === "guerreiro") {
+            ataque = "espada";
+        } else if (this.tipo === "monge") {
+            ataque = "artes marciais";
+        } else if (this.tipo === "ninja") {
+            ataque = "shuriken";
+        } else {
+            ataque = "ataque desconhecido";
+        }
+        console.log(`O ${this.tipo} atacou usando ${ataque}`);
+    }
+}
+
+// Exemplo de uso:
+const heroi1 = new Heroi("Gandalf", 120, "mago");
+const heroi2 = new Heroi("Legolas", 293, "ninja");
+const heroi3 = new Heroi("Aragorn", 210, "guerreiro");
+
+heroi1.atacar();
+heroi2.atacar();
+heroi3.atacar();
